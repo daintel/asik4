b078634e38f6e94f72e7db5f3281d16e3d7ebed5
Amir <saginaevamir@gmail.com>
2026-05-21 22:59:53 +0500

    feat(edge): added Edge class

--- src/Edge.java
+++ src/Edge.java
@@ -1,4 +1,22 @@
-package PACKAGE_NAME;
-
 public class Edge {
+    private int source;
+    private int destination;
+
+    public Edge(int source, int destination) {
+        this.source = source;
+        this.destination = destination;
+    }
+
+    public int getSource() {
+        return source;
+    }
+
+    public int getDestination() {
+        return destination;
+    }
+
+    @Override
+    public String toString() {
+        return "Edge{" + source + " -> " + destination + '}';
+    }
 }
